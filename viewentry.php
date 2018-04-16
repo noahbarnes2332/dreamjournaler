03f5cba226176c538e47253864b1b0d04c3a1ce5
Noah <noahbarnes@u.boisestate.edu>
2018-04-16 13:17:32 -0600

    Working on displaying journal entries

--- viewentry.php
+++ viewentry.php
@@ -18,6 +18,7 @@
  $res = mysqli_query($con,"SELECT summary FROM entries WHERE title='$var_title'");
  $row=mysqli_fetch_array($res);
  $var_summary = $row['summary'];
+ $var_title = $row['title'];
  
 ?>
 
@@ -53,7 +54,7 @@
 		echo "<h2>" . $var_title . "</h2>";
 		echo $var_summary;
 	?>
-	<form action='editentry.php?id=<?php echo $var_title; ?>'>
+	<form action='editentry.php?id=$var_title'>
     <input type="submit" value="Edit Entry" />
 	</form>
 </div>
