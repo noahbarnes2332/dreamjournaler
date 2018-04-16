986cb8c768de9c168f42b68a52e8c73256a7bd8b
Noah <noahbarnes@u.boisestate.edu>
2018-04-16 13:27:12 -0600

    Working on displaying journal entries

--- viewentry.php
+++ viewentry.php
@@ -53,9 +53,10 @@
 	<?php
 		echo "<h2>" . $var_title . "</h2>";
 		echo $var_summary;
+		$hello = "Cattest";
 	?>
 	<form action="editentry.php" method="post">
-	  <input type="text" value="<?php echo $row['title']?>" name="varTitle" />
+	  <input type="text" value="<?php echo $hello?>" name="varTitle" />
       <input type="submit" value="Edit Entry" />
 	</form>
 </div>
