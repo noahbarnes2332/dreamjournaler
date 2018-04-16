861efc9c6ccf879a9b36fc9f7041b105ddaf7c80
Noah <noahbarnes@u.boisestate.edu>
2018-04-15 20:12:05 -0600

    testing text editor

--- welcome.php
+++ welcome.php
@@ -4,5 +4,13 @@
 Welcome <?php echo $_POST["name"]; ?><br>
 Your email address is: <?php echo $_POST["email"]; ?>
 
+<?php
+$Title = $_Post['name'];
+$Entry = $_Post['email'];
+echo "name is $Title";
+echo "email is $Entry";
+?>
+
+
 </body>
 </html>
