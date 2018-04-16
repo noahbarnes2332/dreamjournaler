dd535afec15319be3e57f467f80ef141da1fa183
Noah <noahbarnes@u.boisestate.edu>
2018-04-16 12:53:28 -0600

    Working on displaying journal entries

--- viewentry.php
+++ viewentry.php
@@ -30,6 +30,24 @@
 <link rel="stylesheet" href="css/styles.css">
 <body>
 
+<ul class="navbar">
+  <li class="navbar"><img src="logo/logo.png" height="42" width="42"></li>
+  <li class="navbar"><a href="index.php">Home</a></li>
+  <li class="navbar"><a class="active" href="my-journal.php">My Journal</a></li>
+  <li class="navbar"><a href="learn.php">Learn</a></li>
+  <li class="navbar"><a href="resources.php">Resources</a></li>
+  <li class="navbar"><a href="browse-journals.php">Browse Journals</a></li>
+  <?php 
+  if( !isset($_SESSION['user']) ) {
+	echo '<li class="navbar"><a href="login.php">Login</a></li>';
+	} else {
+	echo '<li class="navbar"><a href="logout.php?logout">Sign Out</a></li>';
+	} 
+  ?>
+</ul>
+
+
+
 <div id="content">
 	<?php
 		echo "<h2>" . $var_title . "</h2>";
