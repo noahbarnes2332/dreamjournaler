5879c16088417c093215542ffbca5b035c233b5f
Noah <noahbarnes@u.boisestate.edu>
2018-04-15 20:20:15 -0600

    testing text editor

--- welcome.php
+++ welcome.php
@@ -6,12 +6,18 @@ Your email address is: <?php echo $_POST["email"]; ?>
 
 <?php
 
-var_dump($_POST["name"]);
+if (isset($_POST['submit'])) {  
 
 $Title = $_Post['name'];
 $Entry = $_Post['email'];
 echo "name is $Title";
 echo "email is $Entry";
+
+}
+
+else {
+	echo "Your form is not submitted yet please fill the form and visit again";
+}
 ?>
 
 
